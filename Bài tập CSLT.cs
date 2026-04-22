b3ee46fea6919f0a827af43cd0a98865fca3fc80
LanTru <trungocbalan@gmail.com>
2026-04-22 13:21:57 +0700

    Add files via upload

---
+++ Bài tập CSLT.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace bt10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Bài tập 10: Đổi số ngày ");
+            int totalDays = 800;
+            int years = totalDays / 365;
+            int weeks = (totalDays % 365) / 7;
+            int days = (totalDays % 365) % 7;
+
+            Console.WriteLine("Số ngày {0} tương ứng với {1} năm, {2} tuần và {3} ngày.", totalDays, years, weeks, days);
+
+            Console.ReadLine();
+        }
+    }
+}
